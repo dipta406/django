c9fb249ee91119797ff0644b8c32279493ab7d1f
shudipta paul <shudiptapaul406@gmail.com>
2022-07-19 04:19:03 +0600

    Test

--- JBerry/home/urls.py
+++ JBerry/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from home import views
 urlpatterns = [
     path('',views.index, name='home'),
+     path('register',views.register, name='register'),
      
 ]
--- JBerry/home/views.py
+++ JBerry/home/views.py
@@ -1,5 +1,24 @@
+import email
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return  render(request, 'index.html')
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            user = User.objects.create_user(
+                first_name=first_name, last_name=last_name, username=username,
+                password=password1, email=email
+            )
+            user.save()
+            return redirect('home')
+        return render(request,'register.html')
+    return  render(request, 'register.html') 
