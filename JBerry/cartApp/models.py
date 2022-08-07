e2c3535047bf1774e25b23ce1880370d8f54e1d0
shudipta paul <shudiptapaul406@gmail.com>
2022-08-08 00:18:09 +0600

    Test

--- JBerry/cartApp/models.py
+++ JBerry/cartApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class cartItem(models.Model):
+    cid = models.IntegerField(auto_created= True ,primary_key= True) 
+    pid = models.IntegerField()
+    qty = models.SmallIntegerField()
+    uid = models.IntegerField()
+    in_cart = models.IntegerField(default=True)
+    
+    def __str__(self) -> str:
+        return f"CartItem(cid = {self.cid}, pid = {self.pid} , qty = {self.qty})"
+    
--- JBerry/cartApp/urls.py
+++ JBerry/cartApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('user/<int:uid>/', views.viewCart, name='ViewCart'),
+    path('user/<int:uid>/', views.viewCart, name='viewCart'),
+     path('addToCart/', views.addToCart, name='addToCart'),
 ]
